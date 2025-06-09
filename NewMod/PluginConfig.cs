66c5a715cf9ebc432d1cf5d9a13ac7b690777a68
goldenguy00 <goldenguy00@gmail.com>
2025-06-09 11:33:58 -0400

    configs

--- NewMod/PluginConfig.cs
+++ NewMod/PluginConfig.cs
@@ -24,14 +24,15 @@ namespace GoldenCoastPlusRevived
             FightChanges = cfg.BindOption(section,
                 "Enable Fight Changes",
                 "Should the changes to Aurelionite's fight be enabled?",
-                true, Extensions.ConfigFlags.RestartRequired);
+                true, 
+                Extensions.ConfigFlags.RestartRequired);
 
             AurelioniteBossVulnerabilityTime = cfg.BindOptionSlider(section,
                 "Vulnerability Time",
                 "Time in seconds that Aurelionite becomes vulnerable per phase",
                 20,
                 10, 120,
-                Extensions.ConfigFlags.ServerSided | Extensions.ConfigFlags.RestartRequired);
+                Extensions.ConfigFlags.ServerSided);
 
             AurelioniteArmorBrokenMult = cfg.BindOptionSlider(section,
                 "Damage taken multiplier debuff",
@@ -41,8 +42,8 @@ namespace GoldenCoastPlusRevived
                 Extensions.ConfigFlags.ServerSided);
 
             AurelioniteBlessingGoldGain = cfg.BindOptionSlider(section,
-                "Aurelionite's Blessing additional gold gain multiplier (0.25 = 25%)",
-                "Adjust the buffs gold gain multiplier",
+                "Aurelionite's Blessing buff additional gold gain multiplier",
+                "Adjust the buffs gold gain multiplier (0.25 = 25%)",
                 0.25f,
                 0f, 1f,
                 Extensions.ConfigFlags.ServerSided);
@@ -50,28 +51,28 @@ namespace GoldenCoastPlusRevived
 
         private static void BindTitanicGreatsword(ConfigFile cfg, string section)
         {
-            BigSword.EnableSword = cfg.Bind(section, "Enable Titanic Greatsword", true, "Should Titanic Greatsword be enabled?");
-            BigSword.SwordDamage = cfg.Bind(section, "Titanic Greatsword Damage", 12.5f, "Adjust Titanic Greatsword's damage coefficient, as a decimal.");
-            BigSword.SwordChance = cfg.Bind(section, "Titanic Greatsword Chance", 5f, "Adjust Titanic Greatsword's chance to proc, as a percentage.");
-            BigSword.SwordProcCoeff = cfg.Bind(section, "Titanic Greatsword Proc Coeff", 1f, "Adjust Titanic Greatsword's proc coeff.");
+            BigSword.EnableSword = cfg.BindOption(section, "Enable Titanic Greatsword", "Should Titanic Greatsword be enabled?", true, Extensions.ConfigFlags.RestartRequired);
+            BigSword.SwordDamage = cfg.BindOptionSlider(section, "Titanic Greatsword Damage", "Adjust Titanic Greatsword's damage coefficient, as a decimal.", 12.5f, 1f, 25f, Extensions.ConfigFlags.ServerSided);
+            BigSword.SwordChance = cfg.BindOptionSlider(section, "Titanic Greatsword Chance", "Adjust Titanic Greatsword's chance to proc, as a percentage.", 5f, 0f, 100f, Extensions.ConfigFlags.ServerSided);
+            BigSword.SwordProcCoeff = cfg.BindOptionSlider(section, "Titanic Greatsword Proc Coeff", "Adjust Titanic Greatsword's proc coeff.", 1f, 0f, 5f, Extensions.ConfigFlags.ServerSided);
         }
         
         private static void BindGoldenKnurl(ConfigFile cfg, string section)
         {
 
-            GoldenKnurl.EnableKnurl = cfg.Bind(section, "Enable Golden Knurl", true, "Should Golden Knurl be enabled?");
-            GoldenKnurl.KnurlHealth = cfg.Bind(section, "Golden Knurl Health", 0.1f, "Adjust how much max health Golden Knurl grants, as a decimal.");
-            GoldenKnurl.KnurlRegen = cfg.Bind(section, "Golden Knurl Regen", 2.4f, "Adjust how much regen Golden Knurl grants.");
-            GoldenKnurl.KnurlArmor = cfg.Bind(section, "Golden Knurl Armor", 20f, "Adjust how much armor Golden Knurl grants.");
+            GoldenKnurl.EnableKnurl = cfg.BindOption(section, "Enable Golden Knurl", "Should Golden Knurl be enabled?", true, Extensions.ConfigFlags.RestartRequired);
+            GoldenKnurl.KnurlHealth = cfg.BindOptionSlider(section, "Golden Knurl Health", "Adjust how much max health Golden Knurl grants, as a decimal.", 0.1f, 0f, 1f);
+            GoldenKnurl.KnurlRegen = cfg.BindOptionSlider(section, "Golden Knurl Regen", "Adjust how much regen Golden Knurl grants.", 2.4f, 0f, 10f);
+            GoldenKnurl.KnurlArmor = cfg.BindOptionSlider(section, "Golden Knurl Armor", "Adjust how much armor Golden Knurl grants.", 20f, 0f, 100f);
 
         }
         
         private static void BindGuardiansEye(ConfigFile cfg, string section)
         {
-            LaserEye.EnableEye = cfg.Bind(section, "Enable Guardians Eye", true, "Should Guardian's Eye be enabled?");
-            LaserEye.EyeDamage = cfg.Bind(section, "Guardians Eye Damage", 25f, "Adjust how much damage Guardian's Eye does, as a decimal.");
-            LaserEye.EyeDamage = cfg.Bind(section, "Guardians Eye Blast Proc Coeff", 1f, "Adjust the proc coefficient of Guardian's Eye blast, as a decimal.");
-            LaserEye.EyeStacksRequired = cfg.Bind(section, "Guardians Eye Stacks Required", 10, "Adjust how many stacks are required for Guardian's Eye to trigger.");
+            LaserEye.EnableEye = cfg.BindOption(section, "Enable Guardians Eye", "Should Guardian's Eye be enabled?", true, Extensions.ConfigFlags.RestartRequired);
+            LaserEye.EyeDamage = cfg.BindOptionSlider(section, "Guardians Eye Damage", "Adjust how much damage Guardian's Eye does, as a decimal.", 2.5f, 0f, 10f, Extensions.ConfigFlags.ServerSided);
+            LaserEye.EyeDamage = cfg.BindOptionSlider(section, "Guardians Eye Blast Proc Coeff", "Adjust the proc coefficient of Guardian's Eye blast, as a decimal.", 1f, 0f, 5f, Extensions.ConfigFlags.ServerSided);
+            LaserEye.EyeStacksRequired = cfg.BindOptionSlider(section, "Guardians Eye Stacks Required", "Adjust how many stacks are required for Guardian's Eye to trigger.", 10, 1, 20, Extensions.ConfigFlags.ServerSided);
         }
     }
 }
